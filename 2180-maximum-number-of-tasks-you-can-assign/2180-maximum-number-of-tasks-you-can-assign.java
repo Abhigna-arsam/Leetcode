b13f614b7b5486867bbd5ab47f2d3ab4a1db5235
Abhigna Arsam <125258286+Abhigna-arsam@users.noreply.github.com>
2025-05-01 21:42:03 +0530

    Time: 1866 ms (19.59%), Space: 55.5 MB (71.02%) - LeetHub

---
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
@@ -0,0 +1,56 @@
+
+
+class Solution {
+    private boolean canAssign(int mid, int[] workers, int[] tasks, int pills, int strength) {
+        TreeMap<Integer, Integer> usableWorkers = new TreeMap<>();
+        int n = workers.length;
+        for (int i = n - mid; i < n; i++)
+            usableWorkers.put(workers[i], usableWorkers.getOrDefault(workers[i], 0) + 1);
+
+        for (int i = mid - 1; i >= 0; --i) {
+            int task = tasks[i];
+            Integer currWorker = usableWorkers.lastKey();
+            
+            if (currWorker < task) {
+                if (pills <= 0)
+                    return false;
+                
+                // Find the weakest worker who can do the task with pill
+                Integer weakestWorker = usableWorkers.ceilingKey(task - strength);
+                if (weakestWorker == null)
+                    return false;
+                
+                pills--;
+
+                usableWorkers.put(weakestWorker, usableWorkers.get(weakestWorker) - 1);
+                if (usableWorkers.get(weakestWorker) == 0)
+                    usableWorkers.remove(weakestWorker);
+
+            } else {
+                usableWorkers.put(currWorker, usableWorkers.get(currWorker) - 1);
+                if (usableWorkers.get(currWorker) == 0)
+                    usableWorkers.remove(currWorker);
+            }
+        }
+        return true;
+}
+
+public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+        Arrays.sort(tasks);
+        Arrays.sort(workers);
+        
+        int low = 0;
+        int high = Math.min(tasks.length, workers.length);
+        int assigned = 0;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canAssign(mid, workers, tasks, pills, strength)) {
+                assigned = mid;
+                low = mid + 1;
+            } else
+                high = mid - 1;
+        }
+        return assigned;
+}
+}
