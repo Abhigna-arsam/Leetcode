816f4a38ded3313eab29c9c4dc5ddf30f201c287
Abhigna Arsam <125258286+Abhigna-arsam@users.noreply.github.com>
2025-05-12 12:18:49 +0530

    Time: 5 ms (92.1%), Space: 44.7 MB (72.01%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
@@ -1,28 +1,38 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        Set<Integer> result = new TreeSet<>();
-        int n = digits.length;
-        
-        for (int i = 0; i < n; i++) {
-            if (digits[i] == 0) continue; 
-            for (int j = 0; j < n; j++) {
-                if (j == i) continue;
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) continue;
-                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
-                    if (num % 2 == 0) {
-                        result.add(num);
-                    }
+        int[] freq = new int[10];
+        for (int digit : digits) {
+            freq[digit]++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+       
+        for (int i = 1; i <= 9; i++) {
+            if (freq[i] == 0) continue;
+            freq[i]--;
+            for (int j = 0; j <= 9; j++) {
+                if (freq[j] == 0) continue;
+                freq[j]--;
+                for (int k = 0; k <= 9; k += 2) {
+                    if (freq[k] == 0) continue;
+                    freq[k]--;
+
+                    int num = i * 100 + j * 10 + k;
+                    result.add(num);
+                    freq[k]++;
                 }
+                freq[j]++;
             }
+            freq[i]++;
         }
-        
+
         int[] ans = new int[result.size()];
-        int idx = 0;
-        for (int num : result) {
-            ans[idx++] = num;
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
         }
+
+        Arrays.sort(ans);
         return ans;
-    
     }
 }
