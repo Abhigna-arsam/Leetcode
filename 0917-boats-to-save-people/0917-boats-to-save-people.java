1f001b3781e62db7f343e22b2534520866c980eb
Abhigna Arsam <125258286+Abhigna-arsam@users.noreply.github.com>
2025-06-05 09:30:56 +0530

    Time: 16 ms (95.05%), Space: 55.1 MB (51.64%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int i = 0;
+        int j = people.length - 1;
+        Arrays.sort(people);
+        int ans = 0;
+        while(i<=j){
+            int sum = people[i]+people[j];
+            if(sum<=limit){
+                i++;
+            }
+           j--;
+           ans++;
+        }
+        return ans;
+    }
+}
