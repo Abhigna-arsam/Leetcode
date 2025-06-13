c4a7d8d75e0acbddcea2429285dc467eb77aeb48
Abhigna Arsam <125258286+Abhigna-arsam@users.noreply.github.com>
2025-06-13 19:43:37 +0530

    Time: 18 ms (40%), Space: 56.7 MB (11%) - LeetHub

---
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
@@ -0,0 +1,38 @@
+class Solution {
+    int n;
+
+    private boolean isValid(int[] nums, int mid, int p) {
+        int i = 0;
+        int pr = 0;
+
+        while (i< n - 1) {
+            if (nums[i + 1] - nums[i] <= mid) {
+                pr++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+
+        return pr >= p;
+    }
+
+    public int minimizeMax(int[] nums, int p) {
+        n = nums.length;
+        Arrays.sort(nums);
+        int l = 0;
+        int r = nums[n - 1] - nums[0];
+        int res = Integer.MAX_VALUE;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (!isValid(nums, mid, p)) {
+                l = mid + 1;                
+            } else {
+                res = mid;
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+
+}
