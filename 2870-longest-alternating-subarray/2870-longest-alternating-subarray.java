b055bcdba9482d3d5c54cfd3de67a65c3ca0ae8f
Abhigna Arsam <125258286+Abhigna-arsam@users.noreply.github.com>
2025-06-15 23:31:11 +0530

    Time: 2 ms (79.55%), Space: 44.8 MB (24.24%) - LeetHub

---
+++ 2870-longest-alternating-subarray/2870-longest-alternating-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int alternatingSubarray(int[] nums) {
+        int l = 0, r = 0;
+        int n = nums.length;
+        int maxLen =-1;
+        while(l<n-1){
+            if(nums[l+1]-nums[l]!=1){
+                l++;
+                continue;
+            }
+            r = l+1;
+            int t = -1;
+            while(r+1<n && nums[r+1]-nums[r] == t){
+                t*=-1;
+                r++;
+            }
+            maxLen = Math.max(maxLen, r - l + 1);
+            l++;
+
+        }
+        return maxLen;
+    }
+}
