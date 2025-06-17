bb5e89b79c01bf13214b1e3fb48502b344e3ca77
Abhigna Arsam <125258286+Abhigna-arsam@users.noreply.github.com>
2025-06-17 19:33:45 +0530

    Time: 2 ms (86.96%), Space: 45.1 MB (38.67%) - LeetHub

--- 1704-special-positions-in-a-binary-matrix/1704-special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/1704-special-positions-in-a-binary-matrix.java
@@ -1,32 +1,59 @@
 class Solution {
     public int numSpecial(int[][] mat) {
-        int m = mat.length;//col
-        int n = mat[0].length;//row
-        int[] rC = new int[m];
-        int[] cC = new int[n];
-        int ans=0;
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                
-                if(mat[i][j]==1){
-                    rC[i]+=1;
-                    cC[j]+=1;
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        int [] rowCount = new int[rows];
+        int [] colCount = new int[cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
                 }
             }
         }
 
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(mat[i][j]==0) continue;
+        int count=0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (mat[i][j] == 1) {
-                    if(rC[i]==1 && cC[j]==1){
-                        ans++;
+                    if (rowCount[i] == 1 && colCount[j] ==1) {
+                        count++;
                     }
                 }
             }
         }
+        
+        return count;
+        // int m = mat.length;//col
+        // int n = mat[0].length;//row
+        // int[] rC = new int[m];
+        // int[] cC = new int[n];
+        // int ans=0;
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+                
+        //         if(mat[i][j]==1){
+        //             rC[i]+=1;
+        //             cC[j]+=1;
+        //         }
+        //     }
+        // }
 
-        return ans;
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(mat[i][j]==0) continue;
+        //         if (mat[i][j] == 1) {
+        //             if(rC[i]==1 && cC[j]==1){
+        //                 ans++;
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return ans;
 
     }
 }
