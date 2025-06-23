3a0e76423adf5198389f826174ff4b32425f5029
Abhigna Arsam <125258286+Abhigna-arsam@users.noreply.github.com>
2025-06-23 10:46:40 +0530

    Time: 196 ms (91.07%), Space: 40.3 MB (100%) - LeetHub

--- 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.java
+++ 2202-sum-of-k-mirror-numbers/2202-sum-of-k-mirror-numbers.java
@@ -1,60 +1,43 @@
 class Solution {
-    private boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        
-        while (left <= right) {
-            if (s.charAt(left++) != s.charAt(right--)) {
+    private int[] digit = new int[100];
+
+    public long kMirror(int k, int n) {
+        int left = 1, count = 0;
+        long ans = 0;
+        while (count < n) {
+            int right = left * 10;
+            for (int op = 0; op < 2; ++op) {
+                for (int i = left; i < right && count < n; ++i) {
+                    long combined = i;
+                    int x = (op == 0 ? i / 10 : i);
+                    while (x > 0) {
+                        combined = combined * 10 + (x % 10);
+                        x /= 10;
+                    }
+                    if (isPalindrome(combined, k)) {
+                        ++count;
+                        ans += combined;
+                    }
+                }
+            }
+            left = right;
+        }
+        return ans;
+    }
+
+    private boolean isPalindrome(long x, int k) {
+        int length = -1;
+        while (x > 0) {
+            ++length;
+            digit[length] = (int) (x % k);
+            x /= k;
+        }
+        for (int i = 0, j = length; i < j; ++i, --j) {
+            if (digit[i] != digit[j]) {
                 return false;
             }
         }
         return true;
-    }
-
-    private String convertToK(long num, int k) {
-        if (num == 0) return "0";
-        
-        StringBuilder res = new StringBuilder();
-        while (num > 0) {
-            res.append(num % k);
-            num /= k;
-        }
-        return res.toString();
-    }
-
-    public long kMirror(int k, int n) {
-        long sum = 0; int l=1;
-
-        while(n>0){
-            int half = (l+1)/2;
-            long minLen = (long) Math.pow(10,half-1);
-            long maxLen = (long)Math.pow(10,half)-1;
-
-            for(long num = minLen; num<=maxLen; num++){
-                String firstHalf = Long.toString(num);
-                String secondHalf = new StringBuilder(firstHalf).reverse().toString();
-
-                String full;
-                if(l%2!=0){
-                    full = firstHalf + secondHalf.substring(1);
-                }
-                else{
-                    full = firstHalf + secondHalf;
-                }
-
-                long number = Long.parseLong(full);
-                String str = convertToK(number,k);
-
-                if(isPalindrome(str)){
-                    sum+=number;
-                    n--;
-                    if (n == 0) {
-                        return sum;
-                    }
-                } 
-            }
-            l++;
-
-        }
-        return sum;
+    
     }
 }
